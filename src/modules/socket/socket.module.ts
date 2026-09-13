87833a12c342b93198776ff4be246b2db1c25ed5
rouzbehghanavatiyan <EMAIL-OF-ROUZBEHGHANAVATIYAN>
2026-09-13 15:06:56 +0330

    fix: gap gpt

--- src/modules/socket/socket.module.ts
+++ src/modules/socket/socket.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatModule } from '../chat/chat.module';
 import { PushNotificationService } from '../chat/pushNotification.service';
+import { GapGptService } from '../chat/gapgpt.service';
 
 @Module({
   imports: [ChatModule],
-  providers: [ChatGateway, PushNotificationService],
+  providers: [ChatGateway, PushNotificationService, GapGptService],
   exports: [ChatGateway],
 })
 export class SocketModule {}
